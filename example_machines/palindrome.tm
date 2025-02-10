0c75b4d8ef7837635889b4ec9a904a36e2e19799
tommilostny <tommilostny@live.com>
2025-02-10 15:06:54 +0100

    Add palindrome Turing machine and update main menu options

---
+++ example_machines/palindrome.tm
@@ -0,0 +1,61 @@
+s
+YES NO
+
+MR s q0
+MR q0 YES
+WB q0 qa a
+WB q0 qb b
+WB q0 qc c
+
+MR qa qa1
+MR qa1 qa1 a
+MR qa1 qa1 b
+MR qa1 qa1 c
+ML qa1 qa2
+
+MR qa2 YES
+MR qa2 NO b
+MR qa2 NO c
+WB qa2 qa3 a
+
+ML qa3 qa4
+ML qa4 qa4 a
+ML qa4 qa4 b
+ML qa4 qa4 c
+MR qa4 q0
+
+
+MR qb qb1
+MR qb1 qb1 a
+MR qb1 qb1 b
+MR qb1 qb1 c
+ML qb1 qb2
+
+MR qb2 YES
+MR qb2 NO a
+MR qb2 NO c
+WB qb2 qb3 b
+
+ML qb3 qb4
+ML qb4 qb4 a
+ML qb4 qb4 b
+ML qb4 qb4 c
+MR qb4 q0
+
+
+MR qc qc1
+MR qc1 qc1 a
+MR qc1 qc1 b
+MR qc1 qc1 c
+ML qc1 qc2
+
+MR qc2 YES
+MR qc2 NO a
+MR qc2 NO b
+WB qc2 qc3 c
+
+ML qc3 qc4
+ML qc4 qc4 a
+ML qc4 qc4 b
+ML qc4 qc4 c
+MR qc4 q0
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     let bin_inv_tm = compile_turing_machine(include_str!("../example_machines/binary_invert.tm"));
     let pow2_a_tm = compile_turing_machine(include_str!("../example_machines/pow2_a.tm"));
     let an_to_anbn_tm = compile_turing_machine(include_str!("../example_machines/an2anbn.tm"));
+    let palindrome_tm = compile_turing_machine(include_str!("../example_machines/palindrome.tm"));
     loop {
         println!("Select a Turing machine to run:");
         println!("1. Binary Inverter");
         println!("2. Power of 2 'a's");
         println!("3. a^n to a^n b^n");
-        println!("4. Exit");
+        println!("4. Palindrome");
+        println!("5. Exit");
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
@@ -29,6 +31,10 @@ fn main() {
             "3" => {
                 println!("Enter string to convert from a^n to a^n b^n:");
                 an_to_anbn_tm.clone()
+            },
+            "4" => {
+                println!("Enter string to check if it is a palindrome:");
+                palindrome_tm.clone()
             },
             _ => break,
         };
